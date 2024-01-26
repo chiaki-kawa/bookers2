00f74dcaf1ffad0f86ca2b33b6f6afa85bea1523
EC2 Default User <ec2-user@ip-172-31-8-165.ap-northeast-1.compute.internal>
2024-01-26 21:35:33 +0000

    Next book create

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :books, dependent: :destroy
+
+  has_one_attached :profile_image
+
+  def get_imager
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
 end
