a8e52e1e2e883899b2437528d3eced5f96fc6747
EC2 Default User <ec2-user@ip-172-31-8-165.ap-northeast-1.compute.internal>
2024-02-11 03:23:33 +0000

    [finish] all page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,7 +16,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to book_path
+    redirect_to books_path
   end
 
   def index
@@ -36,7 +36,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path
+    redirect_to book_path(book.id)
   end
 
   private
---
+++ db/migrate/20240211030944_add_introduction_from_users.rb
@@ -0,0 +1,4 @@
+class AddIntroductionFromUsers < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
